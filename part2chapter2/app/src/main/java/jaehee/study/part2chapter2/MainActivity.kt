d2ffff15563d8a7c9dce8af45ea62165f1687139
이재희 <0jaehui@gmail.com>
2024-11-04 18:45:14 +0900

    1. 녹음기 구현 - MediaRecorder, MediaPlayer 로 녹음 및 재생 2. timer, 파동 표시

--- part2chapter2/app/src/main/java/jaehee/study/part2chapter2/MainActivity.kt
+++ part2chapter2/app/src/main/java/jaehee/study/part2chapter2/MainActivity.kt
@@ -3,23 +3,38 @@ package jaehee.study.part2chapter2
 import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import jaehee.study.part2chapter2.databinding.ActivityMainBinding
+import java.io.IOException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnTimerTickListener {
     companion object {
         private const val REQUEST_CODE_AUDIO_CODE = 200
     }
 
+    // 릴리즈 -> 녹음중 -> 릴리즈
+    // 릴르즈 -> 재생 -> 릴리즈
+    private enum class State {
+        RELEASE, RECORDING, PLAYING
+    }
+
+    private lateinit var timer: Timer
+
     private lateinit var binding: ActivityMainBinding
     private var recorder: MediaRecorder? = null
+    private var player: MediaPlayer? = null
     private var fileName: String = ""
+    private var state: State = State.RELEASE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,36 +42,78 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         fileName = "${externalCacheDir?.absolutePath}/audiorecordtest.3gp"
+        timer = Timer(this)
 
         binding.recordButton.setOnClickListener {
 
-            when {
-                ContextCompat.checkSelfPermission(
-                    this,
-                    android.Manifest.permission.RECORD_AUDIO
-                ) == PackageManager.PERMISSION_GRANTED -> {
-                    // 실제로 녹음을 시작하면 됨
-                    onRecord()
+            when (state) {
+                State.RELEASE -> {
+                    record()
                 }
 
-                ActivityCompat.shouldShowRequestPermissionRationale(
-                    this, android.Manifest.permission.RECORD_AUDIO
-                ) -> {
-
-                    showPermissionRationalDialog()
+                State.RECORDING -> {
+                    onRecord(false)
                 }
 
-                else -> {
-                    ActivityCompat.requestPermissions(
-                        this,
-                        arrayOf(android.Manifest.permission.RECORD_AUDIO),
-                        REQUEST_CODE_AUDIO_CODE
-                    )
+                State.PLAYING -> {
+
                 }
+
             }
 
         }
 
+        binding.playButton.setOnClickListener {
+            when (state) {
+                State.RELEASE -> {
+                    onPlay(true)
+                }
+                else -> {
+                //do nothing
+                }
+
+            }
+
+        }
+
+        binding.stopButton.setOnClickListener{
+            when(state) {
+                State.PLAYING -> {
+                    onPlay(false)
+                }
+                else -> {
+                //do nothing
+                }
+            }
+        }
+
+    }
+
+    private fun record() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.RECORD_AUDIO
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // 실제로 녹음을 시작하면 됨
+                onRecord(true)
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this, android.Manifest.permission.RECORD_AUDIO
+            ) -> {
+
+                showPermissionRationalDialog()
+            }
+
+            else -> {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.RECORD_AUDIO),
+                    REQUEST_CODE_AUDIO_CODE
+                )
+            }
+        }
     }
 
     private fun showPermissionRationalDialog() {
@@ -74,7 +131,116 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun onRecord() {
+    private fun onRecord(start: Boolean) = if (start) {
+        startRecording()
+    } else {
+        stopRecording()
+    }
+
+    private fun onPlay(start: Boolean) = if(start) startPlaying() else stopPlaying()
+
+    private fun startRecording() {
+        state = State.RECORDING
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+//            recorder = MediaRecorder(this)
+//        } else {
+//            recorder = MediaRecorder()
+//        }
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+//            recorder = MediaRecorder(this).apply {
+//
+//            }
+//        } else {
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setOutputFile(fileName)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+                Log.e("APP", "prepare() failed $e")
+            }
+
+            start()
+        }
+
+        binding.waveformView.clearData()
+        timer.start()
+
+        binding.recordButton.setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.baseline_stop_24
+            )
+        )
+
+        binding.recordButton.imageTintList = ColorStateList.valueOf(Color.BLACK)
+        binding.playButton.isEnabled = true
+        binding.playButton.alpha = 0.3f
+
+    }
+
+    private fun stopRecording() {
+        recorder?.apply {
+            stop()
+            release()
+        }
+        recorder = null
+
+        timer.stop()
+
+        state = State.RELEASE
+
+        binding.recordButton.setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.baseline_fiber_manual_record_24
+            )
+        )
+        binding.recordButton.imageTintList =
+            ColorStateList.valueOf(ContextCompat.getColor(this, R.color.red))
+        binding.playButton.isEnabled = true
+        binding.playButton.alpha = 1.0f
+    }
+
+
+    private fun startPlaying() {
+        state = State.PLAYING
+
+        player = MediaPlayer().apply {
+            try {
+                setDataSource(fileName)
+                prepare()
+            } catch (e: IOException) {
+                Log.e("APP", "media player prepare fail $e")
+            }
+            start()
+        }
+
+        binding.waveformView.clearWave()
+        timer.start()
+
+        player?.setOnCompletionListener {
+            stopPlaying()
+        }
+
+        binding.recordButton.isEnabled = false
+        binding.recordButton.alpha = 0.3f
+    }
+
+    private fun stopPlaying() {
+        state = State.RELEASE
+
+        player?.release()
+        player = null
+
+        timer.stop()
+
+        binding.recordButton.isEnabled = true
+        binding.recordButton.alpha = 1.0f
 
     }
 
@@ -106,11 +272,15 @@ class MainActivity : AppCompatActivity() {
         val audioRecordPermissionGranted = requestCode == REQUEST_CODE_AUDIO_CODE
                 && grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
 
-        if(audioRecordPermissionGranted) {
-            onRecord()
+        if (audioRecordPermissionGranted) {
+            onRecord(true)
 
         } else {
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.RECORD_AUDIO) ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    android.Manifest.permission.RECORD_AUDIO
+                )
+            ) {
                 showPermissionRationalDialog()
             } else {
                 showPermissionSettingDialog()
@@ -120,6 +290,19 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    override fun onTick(duration: Long) {
+        val millisecond = duration % 1000
+        val second = (duration / 1000) % 60
+        val minute = (duration / 1000 / 60)
+
+        binding.timerTextView.text = String.format("%02d:%02d.%02d", minute, second, millisecond / 10 )
+
+        if(state == State.PLAYING) {
+            binding.waveformView.replayAmplitude(duration.toInt())
+        } else if (state == State.RECORDING) {
+            binding.waveformView.addAmplitude(recorder?.maxAmplitude?.toFloat() ?: 0f)
+        }
+    }
 
 
 }
---
+++ part2chapter2/app/src/main/java/jaehee/study/part2chapter2/WaveformView.kt
@@ -0,0 +1,87 @@
+package jaehee.study.part2chapter2
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+
+class WaveformView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+): View(context, attrs, defStyleAttr) {
+
+    private val ampList = mutableListOf<Float>()
+    private val rectList = mutableListOf<RectF>()
+
+    private val redPaint = Paint().apply {
+        color = Color.RED
+    }
+
+    private val rectWidth = 10f
+    private var tick = 0
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        for(rectF in rectList) {
+            canvas?.drawRect(rectF, redPaint)
+        }
+    }
+
+    fun addAmplitude(maxAmplitude: Float) {
+
+        ampList.add(maxAmplitude)
+        rectList.clear()
+
+        val maxRect = (this.width / rectWidth).toInt()
+
+        val amps = ampList.takeLast(maxRect)
+
+        for ( (i,amp) in amps.withIndex()) {
+            val rectF = RectF()
+            rectF.top = 0f
+            rectF.bottom = amp
+            rectF.left = i * rectWidth
+            rectF.right = rectF.left + rectWidth
+
+            rectList.add(rectF)
+        }
+
+        invalidate()
+    }
+
+    fun replayAmplitude(duration: Int) {
+        rectList.clear()
+
+        val maxRect = (this.width / rectWidth).toInt()
+        val amps = ampList.take(tick).takeLast(maxRect)
+
+        for ( (i,amp) in amps.withIndex()) {
+            val rectF = RectF()
+            rectF.top = 0f
+            rectF.bottom = amp
+            rectF.left = i * rectWidth
+            rectF.right = rectF.left + rectWidth
+
+            rectList.add(rectF)
+        }
+
+        tick++
+
+        invalidate()
+    }
+
+    fun clearData() {
+        ampList.clear()
+    }
+
+    fun clearWave() {
+        rectList.clear()
+        tick = 0
+        invalidate()
+    }
+}
