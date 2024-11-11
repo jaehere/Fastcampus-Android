ded1228cad876b26e861a9ff621b08624d3f488a
이재희 <0jaehui@gmail.com>
2024-11-11 14:09:30 +0900

    안드로이드 앱에서 서버 구동

--- part2chapter3/app/src/main/java/jaehee/study/part2chapter3/MainActivity.kt
+++ part2chapter3/app/src/main/java/jaehee/study/part2chapter3/MainActivity.kt
@@ -16,36 +16,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val port = 8080
-        val server = ServerSocket(port)
+        Thread{
+            val port = 8080
+            val server = ServerSocket(port)
 
-        val socket = server.accept()
+            val socket = server.accept()
 
-        //스트림은 일방통행, 인풋, 아웃풋 각각 운용해야 한다. 인풋으로 들어온건 아웃풋으로 나간다.
-        socket.getInputStream() //클라이언트로부터 들어오는 스트림 == 클라이언트의 socket.outputStream
-        socket.getOutputStream() //클라이언트에게 데이터를 주는 스트림 == 클라이언트의 socket.inputStream
+            //스트림은 일방통행, 인풋, 아웃풋 각각 운용해야 한다. 인풋으로 들어온건 아웃풋으로 나간다.
+            socket.getInputStream() //클라이언트로부터 들어오는 스트림 == 클라이언트의 socket.outputStream
+            socket.getOutputStream() //클라이언트에게 데이터를 주는 스트림 == 클라이언트의 socket.inputStream
 
-        val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
-        val printer = PrintWriter(socket.getOutputStream())
+            val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+            val printer = PrintWriter(socket.getOutputStream())
 
-        var input: String? = "-1"
+            var input: String? = "-1"
 
-        while(input != null && input != ""){
-            input = reader.readLine()
-        }
+            while(input != null && input != ""){
+                input = reader.readLine()
+            }
 
-        Log.e("SERVER", "READE DATA $input")
+            Log.e("SERVER", "READE DATA $input")
 
-        printer.println("HTTP/1.1 200 OK")
-        printer.println("Content-Type: text/html\r\n")
+            printer.println("HTTP/1.1 200 OK")
+            printer.println("Content-Type: text/html\r\n")
 
-        printer.println("<h1>Hello World</h1>")
-        printer.println("\r\n")
-        printer.flush() //잔여 버퍼링 데이터가 있을 수 있으니 flush로 마저 배출
-        printer.close()
+            printer.println("<h1>Hello World</h1>")
+            printer.println("\r\n")
+            printer.flush() //잔여 버퍼링 데이터가 있을 수 있으니 flush로 마저 배출
+            printer.close()
 
-        reader.close()
+            reader.close()
 
-        socket.close()
+            socket.close()
+        }.start()
+
     }
 }
